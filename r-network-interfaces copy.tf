7ec5aaefe5428e1780cf2d0f378f46b3bbbed74a
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-03-11 08:46:39 +0100

    fixed fmt

--- r-network-interfaces copy.tf
+++ r-network-interfaces copy.tf
@@ -3,12 +3,12 @@
 #--------------------------------------------*--------------------------------------------
 
 resource "azurerm_network_interface" "fgt_untrust_port1" {
-  depends_on            = [ 
-                          azurerm_public_ip.fgt_public_ip
-                          ]
-  name                  = lower(format("%s_%s_%s_%s", "nic", lookup(var.fortinet_settings, "fortigate_vm_name", null) , "p1", "untrust"))
-  location              = var.location
-  resource_group_name   = var.resource_group_name
+  depends_on = [
+    azurerm_public_ip.fgt_public_ip
+  ]
+  name                = lower(format("%s_%s_%s_%s", "nic", lookup(var.fortinet_settings, "fortigate_vm_name", null), "p1", "untrust"))
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "ipconfig_p1_untrust"
@@ -17,7 +17,7 @@ resource "azurerm_network_interface" "fgt_untrust_port1" {
     private_ip_address            = lookup(var.fortinet_settings.network, "untrust_ip", null)
     public_ip_address_id          = azurerm_public_ip.fgt_public_ip["untrust"].id
   }
-    tags = var.tags
+  tags = var.tags
 }
 
 resource "azurerm_network_interface" "fgt_trust_port2" {
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "fgt_trust_port2" {
     private_ip_address_allocation = "Static"
     private_ip_address            = lookup(var.fortinet_settings.network, "trust_ip", null)
   }
-    tags = var.tags
+  tags = var.tags
 }
 
 
--- variables.tf
+++ variables.tf
@@ -3,50 +3,50 @@
 #--------------------------------------------*--------------------------------------------
 
 variable "resource_group_name" {
-    description = "The name of the resource group in which to create the resources."
-    type        = string
+  description = "The name of the resource group in which to create the resources."
+  type        = string
 }
 
 variable "location" {
-    description = "The location/region where the resources will be created."
-    type        = string    
+  description = "The location/region where the resources will be created."
+  type        = string
 }
 
 variable "tags" {
-    description = "A mapping of tags to assign to the resources."
-    type        = map(string)
-    default     = {}
+  description = "A mapping of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
 }
 
 variable "fortinet_prefix" {
-    description = "A prefix for the Fortinet resources."
-    type        = string
-    default     = "FGT"
-  
+  description = "A prefix for the Fortinet resources."
+  type        = string
+  default     = "FGT"
+
 }
 
 variable "fortinet_untrust_subnet_id" {
-    description = "The ID of the subnet to which the untrust network interface will be attached."
-    type        = string
+  description = "The ID of the subnet to which the untrust network interface will be attached."
+  type        = string
 }
 
 variable "fortinet_trust_subnet_id" {
-    description = "The ID of the subnet to which the trust network interface will be attached."
-    type        = string
+  description = "The ID of the subnet to which the trust network interface will be attached."
+  type        = string
 }
 
 variable "fortinet_settings" {
-    description = "The settings for the Fortinet VM."
+  description = "The settings for the Fortinet VM."
 }
 
 variable "fortinet_vm_username" {
-    description = "The username for the Fortinet VM."
-    type        = string
+  description = "The username for the Fortinet VM."
+  type        = string
 }
 
 variable "fortinet_vm_password" {
-    description = "The password for the Fortinet VM."
-    type        = string
+  description = "The password for the Fortinet VM."
+  type        = string
 }
 
 # end
