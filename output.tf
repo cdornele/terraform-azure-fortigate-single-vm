5a2322c666a9199435888d0953765355488177e9
Clairo Dorneles <clairo@clairodorneles.cloud>
2025-03-12 16:22:53 +0100

    upload v1.0.2

--- output.tf
+++ output.tf
@@ -1,6 +1,17 @@
 #--------------------------------------------*--------------------------------------------
-#  Module: [Module Name] - Output
+#  Module: Fortigate - Single VM  - Output
 #--------------------------------------------*--------------------------------------------
+
+output "fortigate_untrust_public_ip" {
+  description = "Fortigate untrust public IP"
+  value = azurerm_public_ip.fgt_public_ip["untrust"].ip_address
+}
+
+output "fortigate_admin_port" {
+    description = "Fortigate admin port"
+    value       = lookup(var.fortinet_settings, "admin_port", "8443")
+
+}
 
 
 # end
